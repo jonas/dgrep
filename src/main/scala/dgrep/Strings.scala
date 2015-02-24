8389f4c8528a6ceb0c7c5a03b23e73afc8f13489
Jonas Fonseca <jonas.fonseca@gmail.com>
2015-02-23 21:09:58 -0500

    Add stringContainsWord to search a line for a given word

--- src/main/scala/dgrep/Strings.scala
+++ src/main/scala/dgrep/Strings.scala
@@ -2,6 +2,9 @@ package dgrep
 
 object Strings {
 
+  def stringContainsWord(word: String)(line: String): Boolean =
+    splitToWords(line) contains word
+
   def splitToWords(line: String): Array[String] =
     line split "[\\s]+" filter (!_.isEmpty)
 
--- src/test/scala/dgrep/Strings.scala
+++ src/test/scala/dgrep/Strings.scala
@@ -6,6 +6,22 @@ class StringsSpec extends FlatSpec with Matchers {
 
   import Strings._
 
+  "stringContainsWord" should "find word a string containing the word" in {
+    stringContainsWord("a")("a b c d") should be (true)
+    stringContainsWord("one-word")("one-word") should be (true)
+    stringContainsWord("last.")("first and	last.") should be (true)
+  }
+
+  it should "find no words in a string without the word" in {
+    stringContainsWord("not-found")("a b c d") should be (false)
+    stringContainsWord("not-found")("first and last.") should be (false)
+  }
+
+  it should "find no words in a string without words" in {
+    stringContainsWord("not-found")("") should be (false)
+    stringContainsWord("not-found")(" 	") should be (false)
+  }
+
   "splitToWords" should "split a line with one or more words" in {
     splitToWords("a b c d") should contain allOf ("a", "b", "c", "d")
     splitToWords("one-word") should contain only ("one-word")
